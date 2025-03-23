5ddce3eb713d1bd47171248963e1b8d53fc11a22
Darshana-Bhasme <darashanabhasme@gmail.com>
2025-03-23 19:20:31 +0530

    Changes: Header Component Completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,52 @@
-
-
+import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react";
+import { Cointener } from "./container";
+import { LogoContainer } from "./logo-container";
+import { NavigationRoutes } from "./navigation-routes";
+import { NavLink } from "react-router-dom";
+import { ProfileContainer } from "./profile-container";
+import { ToggleContainer } from "./toggle-container";
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const { userId } = useAuth(); // Fixed `useAuth` usage
 
-export default Header
+  return (
+   <header 
+      className={cn("w-full border-b duration-150 transition-all ease-in-out")}
+    >
+     <Cointener>
+        <div className="flex items-center gap-4 w-full">
+          {/* Logo Section */}
+          <LogoContainer />
+
+          {/* Navigation Section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink
+                to={"/generate"}
+                className={({ isActive }) =>
+                  cn(
+                    "text-base text-neutral-600",
+                    isActive && "text-neutral-900 font-semibold"
+                  )
+                }
+              >
+                Take An Interview
+              </NavLink>
+            )}
+          </nav>
+
+          <div className="ml-auto flex-items-center gap-6">
+             {/* profile section*/}
+             <ProfileContainer />
+
+             {/*mobile toggle section*/}
+             <ToggleContainer />
+          </div>
+        </div>
+      </Cointener>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/toggle-container.tsx
@@ -0,0 +1,29 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
+
+export const ToggleContainer = () => {
+
+  return (
+  <Sheet>
+    <SheetTrigger>
+        open
+    </SheetTrigger>
+    <SheetContent>
+     <SheetHeader>
+      <SheetTitle>gdshhjsdghsg</SheetTitle>
+      <SheetDescription>
+        dhgscjbxjhbcjhvghjcb 
+      </SheetDescription>
+     </SheetHeader>
+  </SheetContent>
+</Sheet>
+
+)
+}
